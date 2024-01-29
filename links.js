2e620e62dd41c84b05870013b00fe44d09bddda5
winforfree <147972746+winforfree@users.noreply.github.com>
2024-01-29 21:29:19 +0600

    Add files via upload

---
+++ links.js
@@ -0,0 +1,4 @@
+var gpl = "https://www.gnu.org/licenses/gpl-3.0.html";
+var bll = "https://www.gnu.org/licenses/bll-3.0.html";
+var rll = "https://www.gnu.org/licenses/rll-3.0.html";
+var all = "https://www.gnu.org/licenses/all-3.0.html";
