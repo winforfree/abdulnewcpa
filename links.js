98a77ea3007fbcb180342610f11e69c57244a553
winforfree <147972746+winforfree@users.noreply.github.com>
2024-02-01 23:16:01 +0600

    Delete links.js

--- links.js
+++
@@ -1,4 +0,0 @@
-var gpl = "https://vobeluxa.com/cl/4c7d3b726b866abc?p1=&p2=&source=&site=gp";
-var bll = "https://vobeluxa.com/cl/6c3d6f78173d4ba3?p1=&p2=&source=&site=bl";
-var rll = "https://vobeluxa.com/cl/bf20ce26dfbdea0b?p1=&p2=&source=&site=robi";
-var all = "https://vobeluxa.com/cl/b5c792cf2bf7a95f?p1=&p2=&source=&site=airtel";
