4c8f37ab89c1bfa67646bc0703a6660c93382d41
winforfree <147972746+winforfree@users.noreply.github.com>
2024-02-01 23:27:27 +0600

    Update asdasdasdawsda.js

--- asdasdasdawsda.js
+++ asdasdasdawsda.js
@@ -1,5 +1,5 @@
 var gp = "https://rfegyh.com/cl/4f0aaa649a6e8c42?p1=&p2=&source=&site=";
 var bl = "https://rfegyh.com/cl/ff9dd476eb60ab5a?p1=&p2=&source=&site=";
-var robi = "https://rfegyh.com/cl/af8008df64cb9d96?p1=&p2=&source=&site=";
+var robi = "https://rfegyh.com/cl/60c57c7c7d96f61f?p1=&p2=&source=&site=";
 var artl = "https://rfegyh.com/cl/60c57c7c7d96f61f?p1=&p2=&source=&site=";
 // ok
